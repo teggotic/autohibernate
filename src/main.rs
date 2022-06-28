5e968eb7d99c4641efbc70d4cbc99f56eceb2e13
teggotic <teggot.teggot@gmail.com>
2022-06-28 17:40:35 +0300

    Fix deps

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate serde_yaml;
 use std::thread;
 use std::time::Duration;
 
-use std::fs::{self, File};
+use std::fs::{self};
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 
 use std::process::Command;
 
@@ -13,20 +13,12 @@ use std::path::Path;
 
 #[derive(Deserialize)]
 struct Config {
-    batteryInfoDirPath: String,
-}
-
-impl Config {
-    fn clone(&self) -> Config {
-        Config {
-            batteryInfoDirPath: self.batteryInfoDirPath.clone(),
-        }
-    }
+    battery_info_dir_path: String,
 }
 
 fn read_config() -> Config {
     let default_config = Config {
-        batteryInfoDirPath: String::from("/sys/class/power_supply/BAT1"),
+        battery_info_dir_path: String::from("/sys/class/power_supply/BAT1"),
     };
     if let Ok(s) = fs::read_to_string(Path::new("/home/teggot/.config/autohibernate/config.yaml")) {
         let cfg: Config = match serde_yaml::from_str(&s) {
@@ -47,7 +39,7 @@ fn main() {
 
     let max_battery = {
         let contents =
-            match fs::read_to_string(Path::new(&cfg.batteryInfoDirPath).join("energy_full")) {
+            match fs::read_to_string(Path::new(&cfg.battery_info_dir_path).join("energy_full")) {
                 Ok(val) => val,
                 Err(why) => panic!("couldn't read {}", why),
             };
@@ -61,11 +53,11 @@ fn main() {
     loop {
         thread::sleep(Duration::from_secs(1));
 
-        if let Ok(status) = fs::read_to_string(Path::new(&cfg.batteryInfoDirPath).join("status")) {
+        if let Ok(status) = fs::read_to_string(Path::new(&cfg.battery_info_dir_path).join("status")) {
             let status = status.trim();
             if status != "Charging" {
                 let cur_battery = match
-                    fs::read_to_string(Path::new(&cfg.batteryInfoDirPath).join("energy_now"))
+                    fs::read_to_string(Path::new(&cfg.battery_info_dir_path).join("energy_now"))
                 {
                     Ok(contents) => match contents.trim().parse::<i64>() {
                         Ok(val) => val,
@@ -76,7 +68,7 @@ fn main() {
 
                 let percentage = cur_battery as f64 / max_battery as f64 * 100.;
 
-                if percentage <= 4. {
+                if percentage <= 5. {
                     Command::new("systemctl")
                         .arg("hibernate")
                         .output()
